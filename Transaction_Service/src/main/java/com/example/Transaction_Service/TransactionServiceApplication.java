2be33235f2a1480f215883e024ca4ad85a460c13
Your Name <your.email@example.com>
2026-07-15 17:26:08 +0300

    files

---
+++ Transaction_Service/src/main/java/com/example/Transaction_Service/TransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Transaction_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionServiceApplication.class, args);
+	}
+
+}
