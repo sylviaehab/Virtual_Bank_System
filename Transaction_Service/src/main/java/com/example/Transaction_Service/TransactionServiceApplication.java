76a71fe86c6aded6cb0adfe0ee8fe138aebd4023
Your Name <your.email@example.com>
2026-07-16 14:31:16 +0300

    pom file

--- Transaction_Service/src/main/java/com/example/Transaction_Service/TransactionServiceApplication.java
+++ Transaction_Service/src/main/java/com/example/Transaction_Service/TransactionServiceApplication.java
@@ -2,10 +2,8 @@ package com.example.Transaction_Service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class TransactionServiceApplication {
 
 	public static void main(String[] args) {
@@ -13,4 +11,4 @@ public class TransactionServiceApplication {
 		SpringApplication.run(TransactionServiceApplication.class, args);
 	}
 
-}
+} 
