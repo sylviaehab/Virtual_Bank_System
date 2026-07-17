f9fdba0ef1a5462984690d6ca80141d6d9bd2c8a
mariamghalyy4 <es-mariam.ghali2027@alexu.edu.eg>
2026-07-17 04:03:13 +0300

    User Entity Done

---
+++ User_Service/src/main/java/com/example/User_Service/entity/User.java
@@ -0,0 +1,62 @@
+package com.example.User_Service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(
+        name = "users"
+)
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.UUID
+    )
+    private UUID id;
+
+    @Column(
+            nullable = false,
+            length = 50,
+            unique = true
+    )
+    private String username;
+
+    @Column(
+            name = "password_hash",
+            nullable = false,
+            length = 100
+    )
+    private String passwordHash;
+
+    @Column(
+            nullable = false,
+            length = 150,
+            unique = true
+    )
+    private String email;
+
+    @Column(
+            name = "first_name",
+            nullable = false,
+            length = 100
+    )
+    private String firstName;
+
+    @Column(
+            name = "last_name",
+            nullable = false,
+            length = 100
+    )
+    private String lastName;
+
+
+}
