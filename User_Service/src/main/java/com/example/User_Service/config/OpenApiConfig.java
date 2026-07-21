05dc14cdc39987557818f33dc25d724f4dff5c9d
mariamghalyy4 <es-mariam.ghali2027@alexu.edu.eg>
2026-07-21 18:14:26 +0300

    Update user service

---
+++ User_Service/src/main/java/com/example/User_Service/config/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package com.example.User_Service.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Virtual Bank - User Service API",
+                version = "1.0",
+                description = """
+                        User Service for the Virtual Bank System.
+                        
+                        Responsibilities:
+                        - User registration
+                        - User login
+                        - User profile retrieval
+                        """
+        )
+)
+public class OpenApiConfig {
+}
